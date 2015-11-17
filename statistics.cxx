f2177e4890ab3244165705a216906dd1ba163be5
Abdul Malek Hamada <ABDUL.HAMADA@hhu.de>
2015-11-17 10:34:23 +0100

    Update statistics.cxx

--- statistics.cxx
+++ statistics.cxx
@@ -1,21 +1,51 @@
+// hw3_statistics.cpp : Defines the entry point for the console application.
+//Visual Studio .
+
+#include "stdafx.h"
 #include <cmath>
 #include <iostream>
-#include <cstdlib>
+#include <cstdlib> //This header defines several general purpose functions, including dynamic memory management, random number generation, communication with the environment, integer arithmetics, searching, sorting and converting.//
+				   //http://www.cplusplus.com/reference/cstdlib/ //
+#include<ctime>
 
 using namespace std;
+
 //--------------------------------------------------
 // plenty of lines here
+void F_rand(double*x, const int N){
+	for (int i = 0; i < N; i++){
+		
+		x[i] =  rand()/double (RAND_MAX) ;
+
+		}
+}
+void F_stat(double* x, double& mean, double& var, const int N){
+	mean = 0;
+	var = 0;
+	for (int i = 0; i < N; i++){
+		mean += x[i];
+		}
+	mean /= N;
+		for (int i = 0; i < N; i++){
+		var += pow((x[i] - mean), 2);
+			}
+	var /= N;
+}
 //--------------------------------------------------
 
+
 int main(){
-   const int N = 100;
-   double p[N];
-   double mean, var;
+	
+	const int N = 100;
+	double p[N];
+	double mean, var;
+	srand(time(0));
+	// Some lines here....
+	F_rand(p, N);
+	F_stat(p, mean, var, N);
 
-   // Some lines here....
-
-   cout << "Mean = " << mean << endl;
-   cout << "Variance = " << var << endl;
-
-   return 0;
+	cout << "Mean = " << mean << endl;
+	cout << "Variance = " << var << endl;
+	cin.get();
+	return 0;
 }
